971b5dbb31c7f183a3fa2913f9335901b6ac4308
mirjamjagger <mirjamjagger@outlook.com>
2025-01-16 15:42:59 +0100

    Cart design, root layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 
 import './index.css'
 
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import Contact from './pages/Contact'
@@ -12,21 +11,23 @@ import Footer from './components/Footer'
 import NotFound from './pages/NotFound'
 import Cart from './pages/Cart'
 import Productpage from './pages/Productpage'
+import RootLayout from './layouts/Rootlayout'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Products' element={<Products />} />
-        <Route path='Contact' element={<Contact />} />
-        <Route path='/Cart' element={<Cart />} />
-        <Route path='/Productpage' element={<Productpage />} />
 
-        <Route path='*' element={<NotFound />} />
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/Products' element={<Products />} />
+          <Route path='Contact' element={<Contact />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/Productpage' element={<Productpage />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Routes>
-      <Footer />
+
     </BrowserRouter>
   </StrictMode>,
 )
